6cddb7c6732d3d066eedf27bbab15d5aa8a0106f
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-11-06 13:52:56 -0500

    fixed connection

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/countries', {useNewUrlParser: true}, {useUnifiedTopology: true})
 
 let mongoURI = "";
 
@@ -8,7 +7,7 @@ if (process.env.NODE_ENV === "production") {
   } else {
     mongoURI = "mongodb://localhost/countries";
   }
-  
+mongoose.connect(mongoURI, {useNewUrlParser: true}, {useUnifiedTopology: true})
 
 
 
